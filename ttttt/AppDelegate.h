ac0b94c192e04c370cb6d14ece195bdd2fc5cd07
talaa <tamer.alaaeldine@gmail.com>
2012-09-16 20:31:05 +0400

    Initial Commit

---
+++ ttttt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ttttt
+//
+//  Created by Tamer Alaa on 9/16/12.
+//  Copyright (c) 2012 Tamer Alaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
