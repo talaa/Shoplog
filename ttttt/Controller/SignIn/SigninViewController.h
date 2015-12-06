583cb1b139c1df111c15ddd5adb3445aa8a501d5
Mena Bebawy <eng.mena.lorans@gmail.com>
2015-12-06 14:37:59 +0200

    build SignInViewController from scratch

--- ttttt/Controller/SignIn/SigninViewController.h
+++ ttttt/Controller/SignIn/SigninViewController.h
@@ -9,30 +9,11 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 #import <Parse/Parse.h>
-//#import <FBSDKCoreKit/FBSDKCoreKit.h>
-//#import <ParseFacebookUtilsV4/ParseFacebookUtilsV4.h>
 #import "Flurry.h"
 #define Ksingedinalready @"signedalready"
 
 
 @interface SigninViewController : UIViewController
-@property(nonatomic,strong)IBOutlet UIImageView *logoimage;
-@property (nonatomic,strong)IBOutlet UIImageView *userimage;
-@property (nonatomic,strong)IBOutlet UILabel *welcomenote;
-@property (nonatomic,strong)IBOutlet UIButton *logintbutton;
-@property (nonatomic,strong)IBOutlet UIButton *logoutbutton;
 @property (nonatomic,strong)IBOutlet UIButton *Signupbutton;
-@property (nonatomic,strong)IBOutlet UIImageView *wishlistimage;
-@property (nonatomic,strong)IBOutlet UIImageView *shopbagimage;
-@property (nonatomic,strong)IBOutlet UILabel *wishlistcount;
-@property (nonatomic,strong)IBOutlet UILabel *shoplogerstatus;
 
-@property (nonatomic)NSString *facebookID ;
-@property (nonatomic)NSString *username ;
-@property (nonatomic)NSString *userlocation ;
-@property (nonatomic)NSString *usergender ;
-@property (nonatomic)NSString *userbirthday;
-@property (nonatomic)NSString *userrelationship ;
-@property (nonatomic)NSString *useremail ;
-- (IBAction)logoutButtonAction:(id)sender ;
 @end
