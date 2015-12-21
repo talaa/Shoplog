ee9e183bf4688532ff24a8e0bbc0a6757bcabaa2
Mena Lorans <eng.mena.lorans@gmail.com>
2015-12-21 16:26:31 +0200

    create Intro core data class

---
+++ ttttt/Model/CoreData/Intro+CoreDataProperties.m
@@ -0,0 +1,18 @@
+//
+//  Intro+CoreDataProperties.m
+//  ttttt
+//
+//  Created by Mena Bebawy on 12/21/15.
+//  Copyright © 2015 Tamer Alaa. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "Intro+CoreDataProperties.h"
+
+@implementation Intro (CoreDataProperties)
+
+@dynamic dontShowAgain;
+
+@end
